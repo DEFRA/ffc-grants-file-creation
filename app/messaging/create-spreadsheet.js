b0cc5ea173fdeeed4db88bc799c7db6cd26bd1d3
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-04-30 00:48:14 +0100

    Create Spreadsheet upon message receive (#7)
    * Create Spreadsheet upon message receive
    * Hide columns with no data
    * Pass through upload location to file sender

--- app/messaging/create-spreadsheet.js
+++ app/messaging/create-spreadsheet.js
@@ -1,12 +1,79 @@
+const ExcelJS = require('exceljs')
+const { BlobServiceClient } = require('@azure/storage-blob')
+const blobStorageConfig = require('../config/blobStorage')
 const { sendFileCreated } = require('./senders')
+
+async function addWorksheet (workbook, worksheetData) {
+  const worksheet = workbook.addWorksheet(worksheetData.title)
+
+  if (worksheetData.defaultColumnWidth) {
+    worksheet.properties.defaultColWidth = worksheetData.defaultColumnWidth
+  }
+
+  if (worksheetData.protectPassword) {
+    await worksheet.protect(worksheetData.protectPassword)
+  }
+
+  worksheetData.rows.forEach(rowDetails => {
+    const row = worksheet.getRow(rowDetails.row)
+    row.values = rowDetails.values
+
+    if (rowDetails.bold) {
+      row.font = { bold: true }
+    }
+  })
+
+  // Hide rows with no data that fall between rows that do have data
+  if (worksheetData.hideEmptyRows) {
+    const rowsWithData = worksheetData.rows.map(rowDetails => rowDetails.row)
+    const lastRowWithData = Math.max(...rowsWithData)
+
+    for (let rowNum = 1; rowNum < lastRowWithData; rowNum++) {
+      if (!rowsWithData.includes(rowNum)) {
+        const row = worksheet.getRow(rowNum)
+        row.values = ['']
+        row.hidden = true
+      }
+    }
+  }
+}
+
+async function createSpreadsheet (spreadsheetData) {
+  const workbook = new ExcelJS.Workbook()
+  workbook.creator = 'ffc-grants-file-creation'
+  workbook.created = new Date()
+
+  for (const worksheet of spreadsheetData.worksheets) {
+    await addWorksheet(workbook, worksheet)
+  }
+
+  const buffer = await workbook.xlsx.writeBuffer()
+  return buffer
+}
+
+async function uploadSpreadsheet (buffer, filename) {
+  const blobServiceClient = BlobServiceClient.fromConnectionString(blobStorageConfig.connectionStr)
+  const containerClient = blobServiceClient.getContainerClient(blobStorageConfig.containerName)
+  const blockBlobClient = containerClient.getBlockBlobClient(filename)
+
+  // Upload data to the blob
+  const uploadBlobResponse = await blockBlobClient.upload(buffer, buffer.byteLength)
+  console.log('Blob was uploaded successfully')
+  console.log(uploadBlobResponse)
+}
 
 module.exports = async function (msg, submissionReceiver) {
   try {
     const { body } = msg
     console.log('Received message:')
-    console.log(body)
+    console.log(JSON.stringify(body, null, 2))
 
-    await sendFileCreated({ test: 'File has been created' })
+    const spreadSheetBuffer = await createSpreadsheet(body.spreadsheet)
+
+    const filename = body.spreadsheet.filename
+    await uploadSpreadsheet(spreadSheetBuffer, filename)
+
+    await sendFileCreated({ filename, uploadLocation: body.spreadsheet.uploadLocation })
 
     await submissionReceiver.completeMessage(msg)
   } catch (err) {
