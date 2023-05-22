c68011abd831565494a1ab557f7a4233b27d15ef
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-05-23 00:11:42 +0100

    Add real tests

--- test/integration/narrow/unit/index.test.js
+++ test/integration/narrow/unit/index.test.js
@@ -1,3 +1,4 @@
+// Mocks
 jest.mock('../../../../app/server')
 jest.mock('../../../../app/messaging/receivers')
 jest.mock('../../../../app/messaging/create-spreadsheet', () => ({
@@ -6,27 +7,52 @@ jest.mock('../../../../app/messaging/create-spreadsheet', () => ({
     sendEmailToRpa: true,
     protectPassword: mockPassword
 }))
-jest.mock('../../../../app/services/app-insights')
-const { setup } = require('../../../../app/services/app-insights')
+jest.mock('../../../../app/services/app-insights') // why?
+// const { setup } = require('../../../../app/services/app-insights') // why??
+
+// Spies
 const server = require('../../../../app/server')
-server.start = jest.fn(async () => null)
+const mockStartSpy = jest.spyOn(server, 'start').mockImplementation(async () => {
+    console.log('Mock: Server running on %s', server.info.uri)
+})
 const receivers = require('../../../../app/messaging/receivers')
 const mockPassword = 'mock-pwd'
 
 receivers.startProjectDetailsReceiver = jest.fn((a) => null)
 receivers.startContactDetailsReceiver = jest.fn((b) => null)
 receivers.startDesirabilityScoreReceiver = jest.fn((c) => null)
-const indexInit = require('../../../../app/index')
+const mockStartSubmissionReceiver = jest.fn((d) => null)
+receivers.startSubmissionReceiver = mockStartSubmissionReceiver
 
-afterEach(() => {
-    jest.clearAllMocks()
+const logSpy = jest.spyOn(console, 'log').mockImplementation(() => { return null })
+const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { return null })
+
+// describe('get indexInit setup defined', () => {
+//     test('Should be defined', () => {
+//         expect(indexInit).toBeDefined()
+//     })
+//     test('Should call setup once', async () => {
+//         expect(require('../../../../app/index')).toEqual({})
+//         expect(setup).toHaveBeenCalledTimes(0) //wtf???
+
+//     })
+// })
+
+describe('Index', () => {
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    it('Should call server.start', async () => {
+        require('../../../../app/index')
+        expect(mockStartSubmissionReceiver).toHaveBeenCalledTimes(1)
+        expect(mockStartSpy).toHaveBeenCalledTimes(1)
+        expect(logSpy).toHaveBeenCalledWith('Mock: Server running on %s', server.info.uri)
+    })
+
+    it('Should exit on unhandledRejection', async () => {
+        process.emit('unhandledRejection')
+        expect(mockExit).toHaveBeenCalledWith(1)
+    })
 })
-describe('get indexInit setup defined', () => {
-    test('Should be defined', () => {
-    expect(indexInit).toBeDefined()
-})
-    test('Should call setup once', async () => {
-    expect(require('../../../../app/index')).toEqual({})
-    expect(setup).toHaveBeenCalledTimes(0)
-})
-})
+
