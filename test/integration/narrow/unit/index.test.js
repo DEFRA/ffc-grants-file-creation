538fe7627065daaa8a381828b619048eb2477539
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-05-23 00:13:55 +0100

    Clean up, remove redundant tests

--- test/integration/narrow/unit/index.test.js
+++ test/integration/narrow/unit/index.test.js
@@ -7,36 +7,23 @@ jest.mock('../../../../app/messaging/create-spreadsheet', () => ({
     sendEmailToRpa: true,
     protectPassword: mockPassword
 }))
-jest.mock('../../../../app/services/app-insights') // why?
-// const { setup } = require('../../../../app/services/app-insights') // why??
+jest.mock('../../../../app/services/app-insights')
+
+const receivers = require('../../../../app/messaging/receivers')
+const mockPassword = 'mock-pwd'
+const mockStartSubmissionReceiver = jest.fn((d) => null)
+receivers.startProjectDetailsReceiver = jest.fn((a) => null)
+receivers.startContactDetailsReceiver = jest.fn((b) => null)
+receivers.startDesirabilityScoreReceiver = jest.fn((c) => null)
+receivers.startSubmissionReceiver = mockStartSubmissionReceiver
 
 // Spies
 const server = require('../../../../app/server')
 const mockStartSpy = jest.spyOn(server, 'start').mockImplementation(async () => {
     console.log('Mock: Server running on %s', server.info.uri)
 })
-const receivers = require('../../../../app/messaging/receivers')
-const mockPassword = 'mock-pwd'
-
-receivers.startProjectDetailsReceiver = jest.fn((a) => null)
-receivers.startContactDetailsReceiver = jest.fn((b) => null)
-receivers.startDesirabilityScoreReceiver = jest.fn((c) => null)
-const mockStartSubmissionReceiver = jest.fn((d) => null)
-receivers.startSubmissionReceiver = mockStartSubmissionReceiver
-
 const logSpy = jest.spyOn(console, 'log').mockImplementation(() => { return null })
 const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { return null })
-
-// describe('get indexInit setup defined', () => {
-//     test('Should be defined', () => {
-//         expect(indexInit).toBeDefined()
-//     })
-//     test('Should call setup once', async () => {
-//         expect(require('../../../../app/index')).toEqual({})
-//         expect(setup).toHaveBeenCalledTimes(0) //wtf???
-
-//     })
-// })
 
 describe('Index', () => {
     afterEach(() => {
